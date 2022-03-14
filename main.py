befe8bf10afe9779c6a37fd265e2ac101eb4a1a3
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2022-03-13 21:50:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from modules.sshd.sshd_server import SshdServer
 if __name__ == '__main__':
     server = SshdServer()
     address = '0.0.0.0'
-    port = 21022
+    port = 80
     print("Client use: $ ssh admin@{address} -p {port}".format(address=address, port=port))
     server.start(address=address, port=port)
