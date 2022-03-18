5828246a3014d201e2de5d8dcf88e8ec0dc63411
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2022-03-18 18:54:30 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from modules.sshd.sshd_server import SshdServer
-
-if __name__ == '__main__':
-    server = SshdServer()
-    address = '0.0.0.0'
-    port = 8022
-    print("Client use: $ ssh admin@{address} -p {port}".format(address=address, port=port))
-    server.start(address=address, port=port)
